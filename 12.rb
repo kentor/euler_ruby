921148e1e8ca816167ae1f5d818a4cc8c9ce8155
kentor <ken70r@gmail.com>
2012-01-06 18:23:39 -0800

    updated

--- 12.rb
+++ 12.rb
@@ -1,9 +1,16 @@
-index, triangle = 0, 0
-loop do
-  triangle += (index += 1)
-  count = 0
-  1.upto(Math.sqrt(triangle).floor) { |div| count += 1 if triangle % div == 0 }
-  break if count > 251
+require 'prime'
+
+class Integer
+  def divisors_count
+    return 1 if self == 1
+    self.prime_division.map { |a| a.last + 1 }.inject(:*)
+  end
 end
 
-puts triangle
+(1..10**10).inject do |triangle, n|
+  if triangle.divisors_count > 500
+    puts triangle
+    break
+  end
+  triangle + n
+end
--- 17.rb
+++ 17.rb
@@ -1,14 +1,14 @@
 class Integer
-  DICT = {1  => "one",     2  => "two",       3  => "three",    4  => "four",     5  => "five",
-          6  => "six",     7  => "seven",     8  => "eight",    9  => "nine",     10 => "ten",
-          11 => "eleven",  12 => "twelve",    13 => "thirteen", 14 => "fourteen", 15 => "fifteen",
-          16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen", 20 => "twenty",
-          30 => "thirty",  40 => "forty",     50 => "fifty",    60 => "sixty",    70 => "seventy",
-          80 => "eighty",  90 => "ninety",    0  => ""}
+  DICT = { 1  => "one",     2  => "two",       3  => "three",    4  => "four",     5  => "five",
+           6  => "six",     7  => "seven",     8  => "eight",    9  => "nine",     10 => "ten",
+           11 => "eleven",  12 => "twelve",    13 => "thirteen", 14 => "fourteen", 15 => "fifteen",
+           16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen", 20 => "twenty",
+           30 => "thirty",  40 => "forty",     50 => "fifty",    60 => "sixty",    70 => "seventy",
+           80 => "eighty",  90 => "ninety",    0  => "" }
 
   def to_w
     ones = tens = hundreds = thousands = ""
-    _ones, _tens, _hundreds, _thousands = self.to_s.reverse.split("").map(&:to_i)
+    _ones, _tens, _hundreds, _thousands = self.to_s.reverse.chars.map(&:to_i)
 
     thousands = DICT[_thousands] + " thousand" if _thousands && _thousands != 0
     hundreds = DICT[_hundreds] + " hundred" if _hundreds && _hundreds != 0
@@ -25,4 +25,4 @@ class Integer
   end
 end
 
-puts (1..1000).map { |num| num.to_w.tr(' ', '').size }.inject(:+)
+puts (1..1000).map { |n| n.to_w.tr(' ', '').size }.inject(:+)
