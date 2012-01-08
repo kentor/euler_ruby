242ce55f64b4d61a58b04a93abf13c925c21e4bc
kentor <ken70r@gmail.com>
2012-01-07 16:38:28 -0800

    improved 92

--- 92.rb
+++ 92.rb
@@ -1,24 +1,22 @@
 class Integer
-  @@squares = (0..9).map { |n| n*n }
-
-  def sum_square_digits(n = self, sum = 0)
-    until n == 0
-      n, r = n.divmod(10)
-      sum += @@squares[r]
+  def sum_square_digits(q = self, sum = 0)
+    until q == 0
+      q, r = q / 10, q % 10
+      sum += r*r
     end
     sum
   end
 
-  @@cache = [nil] * 568
+  $cache = [false] * 568
 
   (1..567).each do |n|
     seq = n
     seq = seq.sum_square_digits until seq == 1 || seq == 89
-    @@cache[n] = (seq == 1 ? nil : true)
+    $cache[n] = true if seq == 89
   end
 
-  def _89?(n = self)
-    return @@cache[n.sum_square_digits]
+  def _89?
+    return $cache[self.sum_square_digits]
   end
 end
 
