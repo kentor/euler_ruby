c76abef0938d0b81f6c23a97cf756c63290bfa16
kentor <ken70r@gmail.com>
2012-01-08 17:06:11 -0800

    added 72

---
+++ 072.rb
@@ -0,0 +1,23 @@
+require 'prime'
+require 'set'
+
+class Integer
+  Primes = Prime.each(1000).to_a
+
+  def prime_division(n = self, pf = [])
+    Primes.each do |p|
+      break if p*p > self
+      count = 0
+      count, n = count + 1, n / p while n % p == 0
+      pf << [p, count] if count != 0
+    end
+    pf << [n, 1] if n > 1
+    pf
+  end
+
+  def phi
+    self.prime_division.map { |p,k| p**(k-1) * (p-1) }.inject(:*)
+  end
+end
+
+puts (2..1000000).map(&:phi).inject(:+)
