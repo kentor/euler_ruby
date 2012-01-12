45b71ff7677a1be214a019e14ae1077842e9ad94
kentor <ken70r@gmail.com>
2012-01-12 00:32:16 -0800

    clean up

--- 028.rb
+++ 028.rb
@@ -1,15 +1,4 @@
-# 43 44 45 46 47 48 49
-# 42 21 22 23 24 25 26
-# 41 20  7  8  9 10 27
-# 40 19  6  1  2 11 28
-# 39 18  5  4  3 12 29
-# 38 17 16 15 14 13 30
-# 37 36 35 34 33 32 31
-
-sum = 1
-3.step(1001, 2) do |n|
-  sq, diff = n*n, n - 1
-  sum += sq.step(sq - 3*diff, -diff).to_a.inject(:+)
-end
-
-puts sum
+puts 3.step(1001, 2).inject(1) { |memo,n|
+  sq, d = n*n, n-1
+  memo + (sq - 3*d).step(sq, d).inject(:+)
+}
