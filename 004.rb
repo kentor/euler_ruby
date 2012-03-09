ef3a6e1b403e17df2cd21404fdb209f8e4224268
kentor <ken70r@gmail.com>
2012-03-09 02:40:32 -0500

    faster 004

--- 004.rb
+++ 004.rb
@@ -1,1 +1,1 @@
-puts (901..999).to_a.combination(2).map { |x,y| x*y }.select { |n| n == n.to_s.reverse.to_i }.max
+puts (901..999).to_a.combination(2).map { |x,y| x*y }.select { |n| n == "#{n}".reverse!.to_i }.max
