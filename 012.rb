6456859774903aa9201a64bf4c5f66b59adc5b4d
Kenneth Chung <ken70r@gmail.com>
2014-10-21 21:51:27 -0700

    12 - use better block variable names

--- 012.rb
+++ 012.rb
@@ -3,14 +3,14 @@ require 'prime'
 class Integer
   def divisors_count
     return 1 if self == 1
-    self.prime_division.map { |a| a.last + 1 }.inject(:*)
+    self.prime_division.map { |prime_divisor, exponent| exponent + 1 }.inject(:*)
   end
 end
 
-(1..10**10).inject do |triangle, n|
-  if triangle.divisors_count > 500
-    puts triangle
+(1..10**10).inject do |t, n|
+  if t.divisors_count > 500
+    puts t
     break
   end
-  triangle + n
+  t + n
 end
